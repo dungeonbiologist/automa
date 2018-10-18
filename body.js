b5dfd6963e927602144994c4432b4ae1a07db361
Anders Horn <andersdhorn@gmail.com>
2018-10-18 09:57:29 -0700

    refactor

---
+++ body.js
@@ -0,0 +1,101 @@
+function fillBody(context,row){
+  for(var i=0; i<row.length; i++){
+    for(var j=0; j<row[i].length;j++){
+      context.fillStyle= colors[row[i][j]];
+      context.fillRect((j+i/2)%WIDTH*size,i*size,size,size);
+    }
+  }
+}
+function semifillBody(context,row,ruleUsed,whiteList){
+  for(var i=1; i<row.length; i++){
+    for(var j=0; j<row[i].length;j++){
+      if(whiteList[ ruleUsed[i-1][j] ] != undefined){
+        context.fillStyle= colors[row[i][j]];
+        context.fillRect((j+i/2)%WIDTH*size,i*size,size,size);
+      }
+    }
+  }
+}
+function detailBody(context, showRule){
+  context.fillStyle = lightColors[ruleset[showRule]];
+  for(var i=1; i<row.length; i++){
+    for(var j=0; j<row[i].length;j++){
+      //show what this particular rule does.
+      if( ruleUsed[i-1][j] == showRule){
+        context.fillRect((j+i/2)%WIDTH*size,i*size,size,size);
+      }
+    }
+  }
+}
+function drawBody(context, showRule){
+  var place = (showRule==undefined)?states*states: showRule;
+  if(canvases[place].stale){
+    var context2 = canvases[place].getContext('2d');
+    context2.drawImage(canvases[states*states], 0, 0); //draw the default
+    detailBody(context2, showRule); //and add the details
+    canvases[place].stale = false;
+  }
+  context.drawImage(canvases[place], 0, 0);
+}
+function drawPossible(context,possible){
+  for(var i=0; i<row.length; i++){
+    for(var j=0; j<possible[i].length;j++){
+      context.globalAlpha = ""+(1/possible[i][j].length || 1);
+      for(var k=0; k<possible[i][j].length; k++){
+        context.fillStyle= colors[possible[i][j][k]];
+        context.fillRect((j+i/2)%WIDTH*size,i*size,size,size);
+      }
+    }
+  }
+  context.globalAlpha = "1";
+}
+
+function mouseToGrid(x,y){
+  var i= clamp(0,Math.floor(y/size)-1,HEIGHT-2);
+  var j = Math.floor(x/size-1/2-i/2+WIDTH)%WIDTH;
+  clearLog()
+  return {i:i,j:j};
+}
+function mouseDown(e){
+  if(mouse.y<size && mouse.x<WIDTH*size){ //in the first row
+    var x = Math.floor(mouse.x/size);
+    row[0][x] = (row[0][x]+1) % states;
+    fillout(row,ruleset,ruleUsed,countRulesUsed);
+    for(var i=0; i<states*states; i++){
+      canvases[i].stale = true;
+    }
+    fillBody(canvases[states*states].getContext('2d'),row);
+    render();
+  } else if(between(mouse.x/size,0,WIDTH) && between(mouse.y/size,1,HEIGHT-1)){
+    //over field
+    var c = mouseToGrid(mouse.x,mouse.y);
+    var place = ruleUsed[c.i][c.j];
+    if(e.shiftKey){
+        sliders[sliders.active].add(place);
+    }
+    render();
+  }
+}
+function mouseUp (e){
+  differential = false;
+  grabbed = null;
+  grabbedCA = null;
+  render();
+}
+function fillout(row,rule,used,counts){
+  for(var i=0; i<states*states; i++){
+    counts[i]=0;
+  }
+  if(used===undefined){
+    used = [];
+  }
+  for(var i=1; i<HEIGHT; i++){
+    row[i]=[];
+    used[i-1]=[];
+    for(var j=0;j<WIDTH;j++){
+      used[i-1][j] = states*(row[i-1][j])+row[i-1][(j+1)%WIDTH];
+      row[i][j]=rule[ used[i-1][j] ];
+      counts[ used[i-1][j] ]++;
+    }
+  }
+}
---
+++ ruleset.js
@@ -0,0 +1,163 @@
+
+function mouseMoveRuleset(e){
+  var m = mouseOf(e.currentTarget, e);
+  //moused over ruleset
+  var place = states*clamp(0, Math.floor(m.x/size), states-1) + clamp(0, Math.floor(m.y/size), states-1);
+  if(showRule!==place){
+    showRule=place;
+    for(var j=0; j<sliders.length; j++){
+      sliders[j].clearChange();
+      sliders[j].showChange(place);
+    }
+  }
+  render();
+}
+function drawRuleset(context,ruleset){
+  for(var i=0; i<states; i++){
+    for(var j=0; j<states; j++){
+      context.fillStyle = colors[ruleset[i*states+j]];
+      context.fillRect(i*size,j*size,size,size);
+    }
+  } 
+}
+
+function mouseOutRuleset(e){
+  for(var j=0; j<sliders.length; j++){
+    sliders[j].clearChange();
+  }
+  showRule=undefined;
+  render();
+}
+function mouseDownRuleset(e){
+  var m = mouseOf(e.currentTarget, e);
+   //over ruleset
+  var place = states*Math.floor(m.x/size) + Math.floor(m.y/size);
+  if(e.shiftKey){
+    sliders[sliders.active].add(place);
+  } else {
+    var copy = copyVect(ruleset);
+    copy[place] = (ruleset[place]+1)%states;
+    changeRule(copy);
+  }
+  render();
+}
+
+function permuteRuleset(ruleset){
+  var rulesets = [];
+  for(var k=0; k<permutations.length; k++){
+    rulesets[k]=[];
+    for(var i=0; i<states; i++){
+      for(var j=0; j<states; j++){
+        var offset = permutations[k];
+        rulesets[k][states*offset[i]+offset[j]] = offset[ruleset[states*i+j]]; 
+      }
+    }
+  }
+  return rulesets;
+}
+function permutePattern(pattern){
+  var patterns = [];
+  for(var k=0; k<permutations.length; k++){
+    patterns[k]=[];
+    for(var i=0; i<states; i++){
+      for(var j=0; j<states; j++){
+        var offset = permutations[k];
+        for(var l=0; l<states; l++){ //the states within a cell
+          patterns[k][states*(states*offset[i]+offset[j])+offset[l]] = pattern[states*(states*i+j)+l];
+        }
+      }
+    }
+  }
+  return patterns;
+}
+function flipRuleset(ruleset){
+  var flipped = [];
+  for(var i=0; i<states; i++){
+    for(var j=0; j<states; j++){
+      flipped[i+states*j]=ruleset[states*i+j];
+    }
+  }
+  return flipped;
+}
+function flipPattern(pattern){
+  var flipped = [];
+  for(var i=0; i<states; i++){
+    for(var j=0; j<states; j++){
+      for(var k=0; k<states; k++){
+        flipped[states*i + states*states*j + k] = pattern[states*states*i + states*j + k];
+      }
+    }
+  }
+  return flipped;
+}
+function permuteRules(){
+  changeRule(randomElt(permuteRuleset(ruleset)));
+  render();
+}
+function newRule(){
+  var rule = [];
+  for(var i=0; i<states*states; i++){
+    rule[i] = Math.floor(Math.random()*states);
+  }
+  return rule;
+}
+function randomRule(){
+  changeRule(newRule());
+  render();
+}
+function ruleToPattern(rule,flipped,permuted){
+  var unflipped = flipped?0:1;
+  var unpermuted = (permuted==0)?1:0;
+  var pattern=zeroArray(states*states*states);
+  for(var i=0; i<(states*states); i++){
+    pattern[rule[i]+i*states]=1;
+  }
+  pattern.push(unpermuted);
+  pattern.push(unflipped);
+  return pattern;
+}
+
+function minSharedPattern(patterns,cutoff){
+  var result = [];
+  for(var i=0;i <states*states*states; i++){
+    result[i]=Infinity;
+  }
+  for(var p=0; p<patterns.length; p++){
+    for(var i=0;i <states*states*states; i++){
+      result[i] = Math.min(result[i],patterns[p][i]);
+    }
+  }
+  for(var i=0;i <states*states*states; i++){
+    if(result[i]<cutoff){
+      result[i]=0;
+    }
+  }
+  return result;
+}
+function patternToSemirule(pattern){
+  var result = [];
+  for(var i=0; i<states*states; i++){
+    var place=0; 
+    var best=0;
+    for(var j=0; j<states; j++){
+      if(pattern[i*states+j]>best){
+        best = pattern[i*states+j];
+        place=j;
+      }
+    }
+    result[i]=place;
+    if(best==0){
+      result[i]=undefined;
+    }
+  }
+  return result;
+}
+function patternToRule(pattern, ruleset){
+  var rule = patternToSemirule(pattern);
+  for(var i=0; i<states*states; i++){
+    if(rule[i] ==undefined){
+      rule[i]=ruleset[i];
+    }
+  }
+  return rule;
+}
